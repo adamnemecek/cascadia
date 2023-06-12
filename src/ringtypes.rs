9b4c8778a6382928bb0c06614ad9f4dc3bfe1c23
adamnemecek <adamnemecek@gmail.com>
2023-06-11 17:51:57 -0700

    stuff

--- src/ringtypes.rs
+++ src/ringtypes.rs
@@ -5,9 +5,30 @@ pub trait RingClass {
 use std::ops::{
     Add,
     Mul,
+    Sub,
 };
+
+pub trait Inv {
+    type Output;
+    fn inv(&self) -> Self::Output;
+}
+
+pub trait Zero {
+    fn zero() -> Self;
+    fn is_zero(&self) -> bool;
+}
+
+pub trait One {
+    fn one() -> Self;
+    fn is_one(&self) -> bool;
+}
+
 pub trait Ring:
-    Sized + Add<Output = Self> + Mul<Output = Self>
+    Sized
+    + Add<Output = Self>
+    + Sub<Output = Self>
+    + Mul<Output = Self>
+    + Inv<Output = Self>
 {
     //
 }
@@ -16,14 +37,18 @@ pub trait Ring:
 pub trait FractionRingClass: RingClass {
     //
 }
-trait QuotientRingClass: RingClass {
-    //
-}
+// trait QuotientRingClass: RingClass {
+//
+// }
 // trait PolyRingClass <: RingClass {}
 // trait Ring{T<:RingClass} {}
 // trait FractionRing{S<:RingInt,T<:FractionRingClass} <: Ring{T} {}
 // trait QuotientRing{S<:RingInt,T<:QuotientRingClass} <: Ring{T} {}
 // trait Polynomial{S<:Ring,T<:PolyRingClass} <: Ring{T} {}
+
+// pub trait Polynomial<R: Ring>: Ring {
+//     //
+// }
 // trait RingTrait {}
 // trait CommutativeRingTrait <: RingTrait {}
 // trait IntegralDomainTrait <: CommutativeRingTrait {}
@@ -35,8 +60,23 @@ trait QuotientRingClass: RingClass {
 // trait FieldTrait <: EuclidianDomainTrait {}
 // trait AlgebraicallyClosedFieldTrait <: FieldTrait {}
 
-pub struct ZZ<const N: usize> {
+// Integers
+pub struct ZZ(i32);
+
+// impl std::ops::Add for ZZ {
+//     type Output = Self;
+//     fn add(self, rhs: Self) -> Self {
+//         Self(self.0 + rhs.0)
+//     }
+// }
+
+pub struct ZZMod {
     //
 }
 
-struct Hom {}
+// pub struct Frac<
+
+struct Hom<S: Ring, R: Ring, F> {
+    //
+    ph: std::marker::PhantomData<(S, R, F)>,
+}
