26fdea6489ab0a332e1e278c26a1b68f19ed3be0
adamnemecek <adamnemecek@gmail.com>
2023-06-12 09:32:06 -0700

    stuff

--- src/ext.rs
+++ src/ext.rs
@@ -12,3 +12,54 @@ pub trait One {
     fn one() -> Self;
     fn is_one(&self) -> bool;
 }
+
+impl<T: From<usize> + PartialEq> Zero for T {
+    #[inline]
+    fn is_zero(&self) -> bool {
+        self == &Self::zero()
+    }
+
+    #[inline]
+    fn zero() -> Self {
+        0.into()
+    }
+}
+
+impl<T: From<usize> + PartialEq> One for T {
+    #[inline]
+    fn is_one(&self) -> bool {
+        self == &Self::one()
+    }
+
+    #[inline]
+    fn one() -> Self {
+        1.into()
+    }
+}
+
+pub trait DivRem<Rhs = Self>: Sized {
+    // type Output;
+    // fn div_rem(self, rhs: Rhs) -> Self::Output;
+    fn div_rem(self, rhs: Rhs) -> (Self, Self);
+}
+
+macro_rules! impl_div_rem {
+    ($ty: ty) => {
+        impl DivRem for $ty {
+            // type Output = (Self, Self);
+            fn div_rem(self, rhs: Self) -> (Self, Self) {
+                (self / rhs, self % rhs)
+            }
+        }
+    };
+}
+
+impl_div_rem!(usize);
+impl_div_rem!(isize);
+impl_div_rem!(u8);
+impl_div_rem!(u16);
+impl_div_rem!(u32);
+
+// fn gcd(a: usize, b: usize) -> usize {
+//     // a.is
+// }
