01ac08d2d3a7f3159577db6162069c1944dcba0e
Adam Nemecek <adamnemecek@gmail.com>
2023-12-18 18:50:00 -0800

    stuff

--- src/sign.rs
+++ src/sign.rs
@@ -2,9 +2,29 @@
     Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash,
 )]
 pub enum Sign {
-    Pos,
-    Zero,
     Neg,
+    Zero,
+    Pos,
+}
+
+impl Sign {
+    pub fn new(v: isize) -> Self {
+        if v == 0 {
+            Self::Zero
+        } else if v.is_negative() {
+            Self::Neg
+        } else {
+            Self::Pos
+        }
+    }
+
+    pub fn raw(&self) -> isize {
+        match self {
+            Self::Neg => -1,
+            Self::Zero => 0,
+            Self::Pos => 1,
+        }
+    }
 }
 
 impl std::fmt::Debug for Sign {
@@ -26,5 +46,16 @@ impl std::fmt::Display for Sign {
         f: &mut std::fmt::Formatter<'_>,
     ) -> std::fmt::Result {
         write!(f, "{:?}", self)
+    }
+}
+
+impl std::ops::Neg for Sign {
+    type Output = Self;
+    fn neg(self) -> Self::Output {
+        match self {
+            Self::Pos => Self::Neg,
+            Self::Zero => Self::Zero,
+            Self::Neg => Self::Pos,
+        }
     }
 }
